4a61e3716e1a4f863292f970bef07a45c109dc00
Ben Rady <benrady@gmail.com>
2015-01-15 21:48:43 -0600

    removing comment

--- spec/statblocks_spec.rb
+++ spec/statblocks_spec.rb
@@ -55,53 +55,5 @@ describe StatBlocks do
     expect(words).to_not include "408428"
     expect(words).to_not include "paizo.com, lyle hayhurst <sozinsky@gmail.com>, Aug 6, 2011"
   end
-
-#LedfordCR 1/2
-#Male halfling barbarian 1
-#CE Small humanoid (halfling)
-#Init +2; Senses Perception +7
-#Defense
-#AC 15, touch 11, flat-footed 13 (+4 armor, +2 Dex, ­2 rage,
-#+1 size)
-#hp 17 (1d12+5)
-#Fort +7, Ref +3, Will +4; +2 vs. fear
-#Offense
-#Speed 30 ft.
-#Melee greataxe +5 (1d10+4/×3)
-#Special Attacks rage (6 rounds/day)
-#Tactics
-#Before Combat Eager to brawl, only Deandre's intimidation
-#keeps Ledford from rushing in before her signal.
-#During Combat On his first turn, Ledford begins his rage
-#ability, moves to the first PC he can reach, and attacks
-#using power attack. He maintains his rage for the
-#following 6 rounds, but stops using Power Attack if his
-#first two attacks miss a particular PC. If given a choice of
-#targets, Ledford always chooses the biggest, strongest
-#PC. Gleeful at finally being paid to fight people, Ledford
-#shouts out taunts with each blow, followed with a short
-#laugh. He clearly enjoys combat.
-#Morale While raging, Ledford fights to the death. If fatigued
-#at the end of his 6 rounds of rage and reduced to 4 or
-#fewer hit points, Ledford offers to call the fight a draw.
-#Disgusted with his performance, Deandre may strike the
-#failed barbarian.
-#Base Statistics When not raging, Ledford's statistics are
-#AC 17, touch 13, flat-footed 15; hp 15; Melee greataxe +3
-#(1d10+1/×3); Str 13, Con 14; CMB +1, CMD 13; Climb +6
-#Statistics
-#Str 17, Dex 15, Con 18, Int 10, Wis 12, Cha 10
-#Base Atk +1; CMB +3; CMD 13
-#Feats Power Attack
-#Skills Acrobatics +7, Climb +8, Intimidate +4, Perception +7
-#Languages Common, Halfling
-#SQ fast movement
-#Combat Gear potion of cure light wounds; Other Gear
-#masterwork chain shirt, greataxe, block of wax in a metal
-#tin, fine comb with a long handle, bottle of whiskey and four
-#silver cups, 25 gp
-#Rewards: If the PCs survive the ambush, give each
-#player 129 gp.
-#Conclusion
   
 end
